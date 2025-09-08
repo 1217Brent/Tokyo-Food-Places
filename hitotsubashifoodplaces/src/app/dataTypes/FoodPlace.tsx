6fddb0df89960a118b075cbec61fbb84816674cc
Brent Chen <1217brent@Brents-MacBook-Air.local>
2025-09-08 22:56:31 +0900

    deleted stuff

--- hitotsubashifoodplaces/src/app/dataTypes/FoodPlace.tsx
+++
@@ -1,9 +0,0 @@
-export interface FoodPlace {
-    id: number;
-    name: string;
-    image: string;
-    coordinates: [number, number];
-    description: string;
-    link: string;
-    cusine: string[];
-}
